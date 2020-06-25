ec72f576253c3347597158d8a76f5529dde60478
unelee <dbs8891@gmail.com>
2020-06-25 10:10:56 +0900

    20200624

---
+++ 20200624.sql
@@ -0,0 +1,25 @@
+사용자 생성 및 테이블 생성
+
+
+
+--SELECT 실습
+
+SELECT *
+FROM prod;
+
+SELECT prod_id, prod_name
+FROM prod;
+
+
+과제 
+SELECT *
+FROM lprod;
+
+SELECT buyer_id, buyer_name
+FROM buyer;
+
+SELECT *
+FROM csrt;
+
+SELECT mem_id, mem_pass, mem_name
+FROM member;
